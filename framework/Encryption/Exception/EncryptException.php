2578b100d098c4832f0a3c0aa69947222f4e4ba7
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-21 11:55:41 +0300

    Add files via upload

---
+++ framework/Encryption/Exception/EncryptException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью пакета GM Framework.
+ * 
+ * @link https://gearmagic.ru/framework/
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+namespace Gm\Encryption\Exception;
+
+use Gm\Exception\UserException;
+
+/**
+ * Исключение вызванное действием, шифрованием значения.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Gm\Encryption\Exception
+ * @since 2.0
+ */
+class EncryptException extends UserException
+{
+}
---
+++ framework/Encryption/README.md
@@ -0,0 +1,7 @@
+# Компонент Encryption входит в состав GM Framework.
+
+Компонент предоставляют простой и удобный интерфейс для шифрования и дешифрования  текста через OpenSSL с использованием шифрования AES-256 и AES-128.
+
+## Ресурсы
+- [GM Framework](https://apps.gearmagic.ru/framework)
+- [Документация](https://apps.gearmagic.ru/component/framework-encryption)
