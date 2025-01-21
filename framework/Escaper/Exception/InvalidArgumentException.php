d70ec8d8dd325bff3904fc3d480088423c0a88bb
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-21 11:56:28 +0300

    Add files via upload

---
+++ framework/Escaper/Exception/InvalidArgumentException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Этот файл является частью пакета GM Framework.
+ * 
+ * @link https://gearmagic.ru/framework/
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+namespace Gm\Escaper\Exception;
+
+use Gm\Exception;
+
+/**
+ * {@inheritdoc}
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package  Gm\Escaper\Exception
+ * @since 2.0
+ */
+class InvalidArgumentException extends Exception\InvalidArgumentException
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDispatchMessage(): string
+    {
+        return 'Invalid escaper argument';
+    }
+}
