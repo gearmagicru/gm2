2fb8ae23b44e907a50931417106906fe17c258c6
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-03-09 19:16:10 +0300

    Исключение вызываемое при отсутствии файла копирования

---
+++ framework/Import/Exception/FileNotFoundException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Этот файл является частью пакета GM Framework.
+ * 
+ * @link https://gearmagic.ru/framework/
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+namespace Gm\Import\Exception;
+
+/**
+ * Исключение вызываемое при отсутствии файла копирования.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Gm\Import\Exception
+ * @since 2.0
+ */
+class FileNotFoundException extends \Gm\Exception\FileNotFoundException
+{
+}
