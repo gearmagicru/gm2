b206215b7ccb6db342893918a270bfbe0ac42db5
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-11-12 12:54:50 +0300

    Изменения в классе Gm\Db\Sql\Delete
    Устранено "Implicitly nullable parameters are deprecated"

--- framework/Db/Sql/Delete.php
+++ framework/Db/Sql/Delete.php
@@ -121,11 +121,11 @@ class Delete extends AbstractSql
     /**
      * Возвращает необработанное состояние.
      *
-     * @param string|null $key Ключ: 'emptyWhereProtection', 'table', 'set', 'where'.
+     * @param null|string $key Ключ: 'emptyWhereProtection', 'table', 'set', 'where'.
      * 
      * @return mixed
      */
-    public function getRawState(string $key = null): mixed
+    public function getRawState(?string $key = null): mixed
     {
         $rawState = [
             'emptyWhereProtection' => $this->emptyWhereProtection,
