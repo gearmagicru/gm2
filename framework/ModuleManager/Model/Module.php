ef50eaa9dddeb0a98157365e98a20d291d0a5106
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-11-12 13:11:55 +0300

    Изменения в классе Gm\ModuleManager\Model\Module
    Устранено "Implicitly nullable parameters are deprecated"

--- framework/ModuleManager/Model/Module.php
+++ framework/ModuleManager/Model/Module.php
@@ -126,7 +126,7 @@ class Module extends ActiveRecord
      * {@inheritdoc}
      */
     public function fetchAll(
-        string $fetchKey = null, 
+        ?string $fetchKey = null, 
         array $columns = ['*'], 
         Where|Closure|string|array|null $where = null, 
         string|array|null $order = null
